5e0996697b969333d2ec92db05e5998f133bb989
tangbaotrann <tangbaotrann@gmail.com>
2023-01-15 16:29:18 +0700

    updated: added chatbot

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,11 +8,13 @@ import Content from '~/layouts/components/Content';
 import ExploreClinic from '~/components/ExploreClinic';
 import SlideImage from '~/components/SlideImage';
 import HealthInformation from '~/components/HealthInformation';
+import ChatBot from '~/components/ChatBot';
 
 function Home() {
     return (
         <DefaultLayout>
             <ScrollToTop smooth className="scroll-to-top" />
+            <ChatBot />
             <Content>
                 <SlideImage />
                 <ExploreClinic />
