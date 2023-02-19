81cabb3eb5d75cecb1bba9b1db57b62691661b12
tangbaotrann <tangbaotrann@gmail.com>
2023-02-19 09:38:34 +0700

    updated: build chart type line

--- src/components/BarChart/BarChart.js
+++ src/components/BarChart/BarChart.js
@@ -1,23 +1,53 @@
-// import { Bar } from 'react-chartjs-2';
+import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
-// function BarChart() {
-//     return (
-//         <div>
-//             <Bar
-//                 data={{
-//                     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//                     datasets: [
-//                         {
-//                             label: '# of vote',
-//                             data: [12, 5, 6, 4, 9],
-//                         },
-//                     ],
-//                 }}
-//                 height={400}
-//                 width={400}
-//             />
-//         </div>
-//     );
-// }
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
-// export default BarChart;
+export function BarChart() {
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top', //  as const
+            },
+            title: {
+                display: true,
+                text: 'Biểu đồ thống kê sức khỏe hằng ngày của bệnh nhân',
+            },
+        },
+    };
+
+    // labels
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+    // Data
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Glycemic',
+                data: [12, 3, 5, 6, 4, 8, 5],
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: 'BMI',
+                data: [9, 12, 19, 3, 5, 2, 4],
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ],
+    };
+
+    return <Line options={options} data={data} height={180} width={400} />;
+}
