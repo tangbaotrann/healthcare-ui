45cc3d7317556a16937d88333ea232989fa943ed
tangbaotrann <tangbaotrann@gmail.com>
2023-02-25 16:07:28 +0700

    updated: add socket server for doctor manager

---
+++ src/components/TableListNotification/TableListNotification.js
@@ -0,0 +1,14 @@
+// me
+import TitleName from '../TitleName';
+
+function TableListNotification() {
+    return (
+        <>
+            <TitleName>Danh Sách Các Thông Báo Hiện Có Của Bác Sĩ</TitleName>
+
+            {/* Table list */}
+        </>
+    );
+}
+
+export default TableListNotification;
